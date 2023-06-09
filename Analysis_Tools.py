Updated cross-correlation function to normalize from 0 to 1. This fixed a bug in find_max_2d where it broke for negative numbers and also allowed the use of np.where which sped up computation

Analysis_Tools.py
@@ -1,41 +1,14 @@
 from skimage import io
 from scipy import fft
 from scipy import signal
+from scipy import stats
 from matplotlib import pyplot as plt
 import numpy as np
-
-def getAverage(img, u, v, n):
-    """img as a square matrix of numbers"""
-    s = 0
-    for i in range(-n, n + 1):
-        for j in range(-n, n + 1):
-            s += img[u + i][v + j]
-    return float(s) / (2 * n + 1) ** 2
-
-def getStandardDeviation(img, u, v, n):
-    s = 0
-    avg = getAverage(img, u, v, n)
-    for i in range(-n, n + 1):
-        for j in range(-n, n + 1):
-            s += (img[u + i][v + j] - avg) ** 2
-    return (s ** 0.5) / (2 * n + 1)
-
-def zncc(img1, img2, u1, v1, u2, v2, n):
-    stdDeviation1 = getStandardDeviation(img1, u1, v1, n)
-    stdDeviation2 = getStandardDeviation(img2, u2, v2, n)
-    avg1 = getAverage(img1, u1, v1, n)
-    avg2 = getAverage(img2, u2, v2, n)
-
-    s = 0
-    for i in range(-n, n + 1):
-        for j in range(-n, n + 1):
-            s += (img1[u1 + i][v1 + j] - avg1) * (img2[u2 + i][v2 + j] - avg2)
-    return float(s) / ((2 * n + 1) ** 2 * stdDeviation1 * stdDeviation2)
 
 def grayscale_bit_depth(grayscale, bit_depth):
     return (2**bit_depth-1)*(grayscale - grayscale.min())/(grayscale.max() - grayscale.min())
 
-def scan_image(img1, img2, window_size, magnitude=True):
+def scan_image(img1, img2, window_size):
     height1, width1 = np.shape(img1)
     height2, width2 = np.shape(img2)
     if(height1 % window_size > 0 or height2 % window_size>0 or width1 % window_size>0 or width2 % window_size >0):
@@ -43,21 +16,20 @@ def scan_image(img1, img2, window_size, magnitude=True):
     elif(height1!=height2 or width1!=width2):
         #print(f'height1 {height1} height2 {height2}')
         raise ValueError('First and Second Images have different sizes')
-    if(magnitude):
-        cc_matrix = np.zeros((int(height1/window_size), int(width1/window_size)))
-    else:
-        cc_matrix = np.zeros((int(height1/window_size), int(width1/window_size), 2))
-
-    for row in np.arange(0, np.shape(cc_matrix)[0],1):
-        for column in np.arange(0, np.shape(cc_matrix)[1],1):
+        
+    magnitude_matrix = np.zeros((int(height1/window_size), int(width1/window_size)))
+    displacement_matrix = np.zeros((int(height1/window_size), int(width1/window_size), 2))
+    znccs = np.zeros((int(height1/window_size), int(width1/window_size)))
+                         
+    for row in np.arange(0, np.shape(magnitude_matrix)[0],1):
+        for column in np.arange(0, np.shape(magnitude_matrix)[1],1):
             max_coords = find_max_2d(cross_correlate(img1[row*window_size:window_size*(row+1),column*window_size:window_size*(column+1)], 
                                                                     img2[row*window_size:window_size*(row+1),column*window_size:window_size*(column+1)]))
-            if(magnitude):#print(f'max coords: {max_coords}')
-                cc_matrix[row, column] = np.hypot(max_coords[0], max_coords[1])
-            else:
-                cc_matrix[row, column] = (max_coords[0], max_coords[1])
-              
-    return cc_matrix
+            magnitude_matrix[row, column] = np.hypot(max_coords[0], max_coords[1])
+            displacement_matrix[row, column] = (max_coords[0], max_coords[1])
+            znccs[row, column], _ = stats.pearsonr(img1[row*window_size:window_size*(row+1),column*window_size:window_size*(column+1)].flatten(), 
+                                                         img2[row*window_size:window_size*(row+1),column*window_size:window_size*(column+1)].flatten())
+    return (displacement_matrix, magnitude_matrix, znccs)
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
@@ -70,18 +42,23 @@ def cross_correlate(img1, img2):
         img1_autocorr = signal.convolve(img1, img1)
         img2_autocorr = signal.convolve(img2, img2)
         avg_autocorr = 0.5*(img1_autocorr + img2_autocorr)
-
-        return np.flip(signal.convolve(img1, np.flip(img2))) - avg_autocorr
+        cross_correlation = np.flip(signal.convolve(img1, np.flip(img2))) #cross-correlate two images 
+        cc_normed = cross_correlation - avg_autocorr #subtract out the average of each image's autocorrelation to get cleaner peaks
+        cc_normed = (cc_normed - np.min(cc_normed))/(np.max(cc_normed) - np.min(cc_normed)) #scale from 0 to 1
+        return cc_normed
     
 def find_max_2d(matrix):
-    height = np.shape(matrix)[0]
-    width = np.shape(matrix)[1]
-    #print(f'height {height} width {width}')
-    max_coords = [0,0]
-    for x in np.arange(0,width,1):
-        for y in np.arange(0,height,1):
-            #print(f'max_coords: {max_coords}')
-            if(matrix[y,x]>matrix[max_coords[1], max_coords[0]]):
-                max_coords = [x-width, y-height]
-    return max_coords
+    # height = np.shape(matrix)[0]
+    # width = np.shape(matrix)[1]
+    # #print(f'height {height} width {width}')
+    # max_coords = [0,0]
+    # for x in np.arange(0,width,1):
+    #     for y in np.arange(0,height,1):
+    #         #print(f'max_coords: {max_coords}')
+    #         if(matrix[y,x]>matrix[max_coords[1], max_coords[0]]):
+    #             max_coords = [x, y]
+    # return max_coords
+    [y,x] = [np.where(matrix==np.max(matrix))[i][0] for i in [0,1]]
+
+    return [x-len(matrix[0]),y-len(matrix[0,:])]
         

DICAnalysis.py
@@ -15,22 +15,26 @@ class DICAnalysis():
         n = self.n
         window_size = self.window_size
         imgs = self.images
-        cc_stack = np.zeros((n, int(height), int(width)))
+        magnitude_stack = np.zeros((n, int(height), int(width)))
+        displacement_stack = np.zeros((n, int(height), int(width), 2))
         zncc_stack = np.zeros((n, int(height), int(width)))
         
         for p in range(1,n):
             print(f'loop {p} of {n}')
-            cc_matrices = np.zeros((int(height), int(width)))
-            znccs = np.zeros((int(height), int(width)))
-            magnitudes = scan_image(imgs[p-1], imgs[p], window_size)
-            velocities = scan_image(imgs[p-1], imgs[p], window_size, magnitude=False)
+            magnitude_map = np.zeros((int(height), int(width)))
+            displacement_map = np.zeros((int(height), int(width),2))
+            zncc_map = np.zeros((int(height), int(width)))
+            displacements, magnitudes, znccs = scan_image(imgs[p-1], imgs[p], window_size)
+            
             for y in range(0,height, window_size):
                 for x in range(0,width, window_size):
-                    cc_matrices[y:y+window_size,x:x+window_size] = np.ones((window_size, window_size)) * magnitudes[int(y/window_size),int(x/window_size)]
-                    znccs[y:y+window_size,x:x+window_size] = np.ones((window_size, window_size))
-            cc_stack[p] = cc_matrices
-            zncc_stack[p] = znccs
-        return cc_stack
+                    magnitude_map[y:y+window_size,x:x+window_size] = np.ones((window_size, window_size)) * magnitudes[int(y/window_size),int(x/window_size)]
+                    displacement_map[y+int(window_size/2),x+int(window_size/2)] = displacements[int(y/window_size),int(x/window_size)] #create sparse matrix same size as original image and center displacement vector in window 
+                    zncc_map[y:y+window_size,x:x+window_size] = np.ones((window_size, window_size)) * znccs[int(y/window_size),int(x/window_size)]
+            magnitude_stack[p] = magnitude_map
+            displacement_stack[p] = displacement_map
+            zncc_stack[p] = zncc_map
+        return (displacement_stack, magnitude_stack, zncc_stack)
         # plt.imshow(imgs[p], cmap='Greys', origin='lower')
         # plt.imshow(cc_matrices, cmap='jet', alpha=0.25, origin='lower')
         # plt.colorbar()